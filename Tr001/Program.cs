7654151f34178d337251b24c1d1d14af69ffa382
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-15 01:27:22 +0300

    Д/З Базовые алгоритмы

---
+++ Tr001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число:");
+int abc = Convert.ToInt32(Console.ReadLine());
+int value = abc;
+int a = value / 100;
+int ab = value / 10;
+int b = ab - (a * 10);
+Console.WriteLine(b);
---
+++ Tr004/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число n:");
+int abc = int.Parse(Console.ReadLine());
+int value = abc;
+if (value < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+    return;
+}
+while (value > 1000)
+{
+    value = value / 10;
+}
+value = value % 10;
+Console.WriteLine(value);
