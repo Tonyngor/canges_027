effa96f77e7c92c19dcea8aca5f5c570495c61d1
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-15 18:30:54 +0300

    + папка 005

---
+++ Tr005/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] Array = new int[] { 1,2,3,4,5,6,7 };
+int Index = 0;
+int size = Array.Length;
+if (size < 3)
+{
+    Console.WriteLine("Нет третьей цифры");
+}
+else
+{
+    while (Index < 2)
+    {
+        Index = Index + 1;
+    }
+    Console.WriteLine(Array[Index]);
+}
