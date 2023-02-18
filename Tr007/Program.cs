d50383624e70a3cc83cea6a845cbce0901c77d50
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-18 19:53:57 +0300

    new folder 007, 008

---
+++ Tr007/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход координаты двух точек
+        // и находит расстояние между ними в 3D пространстве.
+        Console.WriteLine("Введите X1: ");
+        double x1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите y1: ");
+        double y1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите z1: ");
+        double z1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите X2: ");
+        double x2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите y2: ");
+        double y2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите z2: ");
+        double z2 = Convert.ToInt32(Console.ReadLine());
+
+        double resalt = Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+        Console.WriteLine(resalt);
+    }
+}
---
+++ Tr008/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход число (N) 
+        // и выдаёт таблицу кубов чисел от 1 до N.
+        Console.WriteLine("Введите положительное число");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int value = 0;
+        int x = 0;
+        if (n < 1)
+        {
+            Console.WriteLine("Это неправильное число");
+        }
+        else
+        {
+            while (value < n)
+            {
+                value = value + 1;
+                x = value * value * value;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
