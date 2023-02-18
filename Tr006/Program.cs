bc35ef7a3efc919fcb5dbf9df6509f706baa5a49
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-18 13:20:24 +0300

    new project 006

---
+++ Tr006/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+        // является ли оно палиндромом.
+        Console.WriteLine("Введите пятизначное число:");
+        int abcde = Convert.ToInt32(Console.ReadLine());
+        int value = abcde;
+        int a = value / 10000;
+        int b = value / 1000 % 10;
+        int e = value % 10;
+        int d = value % 100 / 10;
+        if (a == e && b == d)
+        {
+            Console.WriteLine("Это палиндром");
+        }
+        else
+        {
+            Console.WriteLine("Число не является палиндромом");
+        }
+    }
+}
