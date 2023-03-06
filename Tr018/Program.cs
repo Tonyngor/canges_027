e216e2ff4a56790f4427d797a8ca02d3ad52d64b
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-03-06 11:28:41 +0300

    018 - 020

---
+++ Tr018/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Пользователь вводит с клавиатуры M чисел. 
+        // Посчитайте, сколько чисел больше 0 ввёл пользователь.
+        // Подзадачи:
+        // Ограничить количество M чисел
+        // Обозначить пределы максимального и минимального числа
+        Console.WriteLine("Введите количество чисел (m): ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int result = 0;
+        if (m < 1 || m > 10)
+        {
+            Console.WriteLine("Неправильное число. Начните заново ");
+            return;
+        }
+        Console.WriteLine("Введите " + m + " чисел");
+        while (m > 0)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number > 9 || number < -9)
+
+            {
+                Console.WriteLine("Неправильное число. Начните заново");
+                return;
+            }
+            if (number > 0)
+            {
+                result = result + 1;
+            }
+            m--;
+        }
+
+        Console.WriteLine("Количество чисел больше нуля : " + result);
+    }
+}
